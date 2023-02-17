de793940ba10c3b620d3ee49a790a41019a3a36c
bizyaeva94 <bizyaeva94@gmail.com>
2023-02-17 20:33:27 +0530

    add locators and methods for TestCheckBox

--- locators/locators.py
+++ locators/locators.py
@@ -11,3 +11,12 @@ class TextBoxLocators:
     OUTPUT_EMAIL = (By.CSS_SELECTOR, "#output #email")
     OUTPUT_CURRENT_ADDRESS = (By.CSS_SELECTOR, "#output #currentAddress")
     OUTPUT_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "#output #permanentAddress")
+
+
+class CheckBoxLocators:
+    EXPAND_ALL = (By.CSS_SELECTOR, "button[title='Expand all']")
+    CHECKBOX_TITLE = (By.CSS_SELECTOR, "span.rct-title")
+    EXPANDED_LIST = (By.CSS_SELECTOR, ".rct-node-expanded")
+    CHECKED_ITEM = (By.XPATH, "//*[contains(@class, 'rct-icon-check')]/ancestor::label/span[@class='rct-title']")
+    SELECTED_RESULT = (By.CSS_SELECTOR, "#result .text-success")
+
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,7 @@
 from locators.locators import TextBoxLocators
+from locators.locators import CheckBoxLocators
 from pages.base_page import BasePage
+import random
 
 
 class TextBoxPage(BasePage):
@@ -17,3 +19,31 @@ class TextBoxPage(BasePage):
         output_current_address = self.browser.find_element(*TextBoxLocators.OUTPUT_CURRENT_ADDRESS).text.split(':')[1]
         output_permanent_address = self.browser.find_element(*TextBoxLocators.OUTPUT_PERMANENT_ADDRESS).text.split(':')[1]
         return output_name, output_email, output_current_address, output_permanent_address
+
+
+class CheckBoxPage(BasePage):
+
+    def expand_all(self):
+        self.browser.find_element(*CheckBoxLocators.EXPAND_ALL).click()
+        self.elements_are_present(*CheckBoxLocators.EXPANDED_LIST)
+
+    def click_random_checkboxes(self):
+        item_list = self.browser.find_elements(*CheckBoxLocators.CHECKBOX_TITLE)
+        for _ in range(random.randint(1, 10)):
+            item = item_list[random.randint(1, 16)]
+            self.scroll_to_element(item)
+            item.click()
+
+    def get_checked_checkboxes(self):
+        checked_items = self.browser.find_elements(*CheckBoxLocators.CHECKED_ITEM)
+        checked_titles = []
+        for item in checked_items:
+            checked_titles.append(item.text)
+        return str(checked_titles).lower()
+
+    def get_output_result(self):
+        result = self.browser.find_elements(*CheckBoxLocators.SELECTED_RESULT)
+        result_titles = []
+        for item in result:
+            result_titles.append(item.text)
+        return str(result_titles).lower()
