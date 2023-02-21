0d732a2f67ce0d38a74cb41476a009e179d9c095
bizyaeva94 <bizyaeva94@gmail.com>
2023-02-21 17:23:42 +0530

    fix test in check_box

--- tests/test_check_box.py
+++ tests/test_check_box.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 class TestCheckBox:
-    def test_fill_all_fields(self, browser):
+    def test_check_random_boxes(self, browser):
         link = 'https://demoqa.com/checkbox'
         page = CheckBoxPage(browser)
         page.open(link)
@@ -12,3 +12,4 @@ class TestCheckBox:
         checked_titles = page.get_checked_checkboxes()
         result_titles = page.get_output_result()
         assert checked_titles == result_titles, "Output result does not match checked checkboxes"
+
